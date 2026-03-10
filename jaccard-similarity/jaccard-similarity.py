474d45aad485e05370b34d6a3dda0c5e54c156fd
Harshita Jha <94231426+HarshitaJ02@users.noreply.github.com>
2026-03-10 16:10:49 +0530

    Update solution for jaccard-similarity

--- jaccard-similarity/jaccard-similarity.py
+++ jaccard-similarity/jaccard-similarity.py
@@ -2,14 +2,14 @@ def jaccard_similarity(set_a, set_b):
     """
     Compute the Jaccard similarity between two item sets.
     """
-    if set_a == [] and set_b == []:
+    if not set_a and  not set_b:
         return 0.0
     set1 = set(set_a)
     set2 = set(set_b)
     un_s= set1 | set2
     in_s=  set2 & set1
 
-    if un_s == 0:
-            return null
-
-    return abs(len(in_s)) / abs(len(un_s))
+    if not un_s: 
+            return None
+# len is always non negative
+    return len(in_s) / len(un_s)
