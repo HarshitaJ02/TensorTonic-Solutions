738883f3bffb2bcb7f3b7951d4f1d5cfe7ceacda
Harshita Jha <94231426+HarshitaJ02@users.noreply.github.com>
2026-03-10 17:43:08 +0530

    Update solution for softmax-function

--- softmax-function/softmax-function.py
+++ softmax-function/softmax-function.py
@@ -7,9 +7,8 @@ def softmax(x):
     For 2D, compute row-wise softmax.
     """
     # Write code here
-    x = np.atleast_2d(x).astype(float)    
-    e = np.exp(x- np.max(x, axis =1, keepdims = True)) 
-    s = e/np.sum(e, axis= 1, keepdims = True)
-
-    return s if x.shape[0]>1 else s[0]
+    x = np.asarray(x, dtype=float) 
+    e = np.exp(x- np.max(x, axis =-1, keepdims = True)) 
+    s = e/np.sum(e, axis= -1, keepdims = True)
+    return s
     
